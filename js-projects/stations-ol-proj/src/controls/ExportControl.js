089b8bb2d5f12b21afb41a4cd88ab5331db49839
Roger Groth <roger.groth@gis.lu.se>
2018-01-10 13:39:01 +0100

    Allow export of map w/wo legend

--- js-projects/stations-ol-proj/src/controls/ExportControl.js
+++ js-projects/stations-ol-proj/src/controls/ExportControl.js
@@ -30,89 +30,43 @@ export default class ExportControl extends Control {
 		});
 		this.element.appendChild(switchBtn);
 
-		const printBtn = document.createElement('button');
-		printBtn.setAttribute('class', 'dl-action');
-		printBtn.innerHTML = "Export map to image";
+		const printWithLegendBtn = document.createElement('button');
+		printWithLegendBtn.setAttribute('class', 'dl-action');
+		printWithLegendBtn.innerHTML = "Export map with legend";
 
-		printBtn.addEventListener('click', () => {
+		printWithLegendBtn.addEventListener('click', () => {
 			const map = this._map;
 
-			if (map === undefined) {
-				throw new Error("Map is undefined");
-			}
-
-			try {
-				const isFileSaverSupported = !!new Blob;
-			} catch (e) {
-				throw new Error("Blob is not supported in your browser");
-			}
-
 			map.once('postcompose', event => {
-				const mapCanvas = event.context.canvas;
-				const canvas = document.createElement('canvas');
-				canvas.width = mapCanvas.width;
-				canvas.height = mapCanvas.height;
-				const toggleLayers = map.getLayers().getArray().filter(l => l.get('layerType') === 'toggle');
-
-				const ctx = canvas.getContext('2d');
-				// Copy map image to canvas
-				ctx.drawImage(mapCanvas, 0, 0);
-				// Draw legend rectangle
-				ctx.fillStyle = 'rgb(211, 211, 211)';
-				ctx.border = '1px solid black';
-				ctx.fillRect(1, 1, 180, 95);
-				ctx.strokeRect(1, 1, 180, 95);
-
-				let dx;
-				let dy = 15;
-
-				// Legend text style
-				ctx.font = '14px Arial';
-				ctx.fillStyle = 'black';
-				ctx.textBaseline = 'middle';
-
-				toggleLayers.forEach(l => {
-					const style = l.getStyle();
-					const image = style ? style.getImage() : undefined;
-					const canvasImage = image ? image.canvas_ : undefined;
-
-					if (canvasImage) {
-						// Id is set in LayerControl
-						const txt = canvasImage.id.replace('canvas', '').replace('_', ' ');
-						// Create a copy of legend icon
-						const canvasImageCopy = document.createElement('canvas');
-						canvasImageCopy.width = canvasImage.width;
-						canvasImageCopy.height = canvasImage.height;
-						const canvasImageCopyCtx = canvasImage.getContext('2d');
-						canvasImageCopyCtx.drawImage(canvasImage, 0, 0);
-
-						const imageData = canvasImageCopyCtx.getImageData(0, 0, canvasImage.width, canvasImage.height);
-
-						// Set gray background on legend icon
-						setBgColor(imageData.data, 211);
-
-						// Add legend icon and text
-						dx = 13 - canvasImage.width / 2;
-						ctx.putImageData(imageData, dx, dy - canvasImage.height / 2);
-						ctx.fillText(txt, 23, dy);
-
-						dy += 22;
-					}
-				});
-
-				if (navigator.msSaveBlob) {
-					navigator.msSaveBlob(canvas.msToBlob(), 'map.png');
-				} else {
-					canvas.toBlob(blob => {
-						saveAs(blob, 'map.png');
-					});
-				}
+				exportMap(map, event, true);
 			});
 			this._map.renderSync();
 			return false;
 		});
 
-		content.appendChild(printBtn);
+		content.appendChild(printWithLegendBtn);
+
+		const spacing = document.createElement('div');
+		spacing.setAttribute('style', 'height:3px;');
+		spacing.innerHTML = "&nbsp;";
+		content.appendChild(spacing);
+
+		const printWithoutLegendBtn = document.createElement('button');
+		printWithoutLegendBtn.setAttribute('class', 'dl-action');
+		printWithoutLegendBtn.innerHTML = "Export map without legend";
+
+		printWithoutLegendBtn.addEventListener('click', () => {
+			const map = this._map;
+
+			map.once('postcompose', event => {
+				exportMap(map, event, false);
+			});
+			this._map.renderSync();
+			return false;
+		});
+
+		content.appendChild(printWithoutLegendBtn);
+
 		this.element.appendChild(content);
 	}
 
@@ -121,6 +75,82 @@ export default class ExportControl extends Control {
 		this._map = map;
 	}
 }
+
+const exportMap = (map, event, withLegend) => {
+	if (map === undefined) {
+		throw new Error("Map is undefined");
+	}
+
+	try {
+		const isFileSaverSupported = !!new Blob;
+	} catch (e) {
+		throw new Error("Blob is not supported in your browser");
+	}
+
+	const mapCanvas = event.context.canvas;
+	const canvas = document.createElement('canvas');
+	canvas.width = mapCanvas.width;
+	canvas.height = mapCanvas.height;
+
+	const ctx = canvas.getContext('2d');
+	// Copy map image to canvas
+	ctx.drawImage(mapCanvas, 0, 0);
+
+	if (withLegend) {
+		const toggleLayers = map.getLayers().getArray().filter(l => l.get('layerType') === 'toggle');
+
+		// Draw legend rectangle
+		ctx.fillStyle = 'rgb(211, 211, 211)';
+		ctx.border = '1px solid black';
+		ctx.fillRect(1, 1, 180, 95);
+		ctx.strokeRect(1, 1, 180, 95);
+
+		let dx;
+		let dy = 15;
+
+		// Legend text style
+		ctx.font = '14px Arial';
+		ctx.fillStyle = 'black';
+		ctx.textBaseline = 'middle';
+
+		toggleLayers.forEach(l => {
+			const style = l.getStyle();
+			const image = style ? style.getImage() : undefined;
+			const canvasImage = image ? image.canvas_ : undefined;
+
+			if (canvasImage) {
+				// Id is set in LayerControl
+				const txt = canvasImage.id.replace('canvas', '').replace('_', ' ');
+				// Create a copy of legend icon
+				const canvasImageCopy = document.createElement('canvas');
+				canvasImageCopy.width = canvasImage.width;
+				canvasImageCopy.height = canvasImage.height;
+				const canvasImageCopyCtx = canvasImage.getContext('2d');
+				canvasImageCopyCtx.drawImage(canvasImage, 0, 0);
+
+				const imageData = canvasImageCopyCtx.getImageData(0, 0, canvasImage.width, canvasImage.height);
+
+				// Set gray background on legend icon
+				setBgColor(imageData.data, 211);
+
+				// Add legend icon and text
+				dx = 13 - canvasImage.width / 2;
+				ctx.putImageData(imageData, dx, dy - canvasImage.height / 2);
+				ctx.fillText(txt, 23, dy);
+
+				dy += 22;
+			}
+		});
+	}
+
+	if (navigator.msSaveBlob) {
+		navigator.msSaveBlob(canvas.msToBlob(), 'map.png');
+	} else {
+		canvas.toBlob(blob => {
+			saveAs(blob, 'map.png');
+		});
+	}
+};
 
 const setBgColor = (data, val) => {
 	for (let i=0; i<data.length; i+=4){
